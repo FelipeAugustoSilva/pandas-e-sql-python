048e5784dccd9dcd3276b38b5d4f821ff9c6ad9b
FelipeAugusto <94065571+FelipeAugustoSilva@users.noreply.github.com>
2024-09-03 04:37:55 -0300

    Aprendendo pandas

--- dataframes.py
+++ dataframes.py
@@ -306,3 +306,170 @@ print(dfff.fillna(0)) # os valores em NaN seram substituidos por 0, podendo usar
 #0  1.0  5.0  1
 #1  2.0  0.0  2
 #2  0.0  0.0  3
+
+print(dfff['A'].mean()) # vai ser impresso a media da coluna "A":
+#1.5
+
+
+print(dfff['A'].fillna(value=dfff['A'].mean()))#os campos com valores NaN na coluna A vao ser preenchidos com o valor da media:
+#0    1.0
+#1    2.0
+#2    1.5
+#Name: A, dtype: float64
+
+print(dfff.ffill()) # vai preencher os valores de NaN com o ultimo valor proximo:
+ #    A    B  C
+#0  1.0  5.0  1
+#1  2.0  5.0  2
+#2  2.0  5.0  3
+
+
+data = {'Classe':['Júnior', 'Júnior', 'Pleno', 'Pleno', 'Sênior', 'Sênior'],
+        'Nome':['Jorge', 'Carlos', 'Roberta', 'Patricia', 'Bruno', 'Vera'],
+        'Venda':[200,120,340,124,243,350]}
+
+rp = pd.DataFrame(data)
+print(rp) # vai ser impresso:
+#   Classe      Nome  Venda
+#0  Júnior     Jorge    200
+#1  Júnior    Carlos    120
+#2   Pleno   Roberta    340
+#3   Pleno  Patricia    124
+#4  Sênior     Bruno    243
+#5  Sênior      Vera    350
+
+group = rp.groupby("Classe")
+group.sum()
+print(group)#Vai ser impresso:
+#                   Nome  Venda
+#Classe
+#Júnior      JorgeCarlos    320
+#Pleno   RobertaPatricia    464
+#Sênior        BrunoVera    593
+
+group = rp.groupby("Classe")
+mean_result = group['Venda'].mean()
+print(mean_result)#Vai ser impresso:
+#Classe
+#Júnior    160.0
+#Pleno     232.0
+#Sênior    296.5
+#Name: Venda, dtype: float64
+
+mingroup = rp.groupby("Classe").min()
+print(mingroup)#Vai ser impresso:
+#            Nome  Venda
+#Classe
+#Júnior    Carlos    120
+#Pleno   Patricia    124
+#Sênior     Bruno    243
+
+maxgroup = rp.groupby("Classe").max()
+print(maxgroup)#Vai ser impresso:
+#           Nome  Venda
+#Classe
+#Júnior    Jorge    200
+#Pleno   Roberta    340
+#Sênior     Vera    350
+
+df1 = pd.DataFrame({'A': ['A0', 'A1', 'A2', 'A3'],
+                        'B': ['B0', 'B1', 'B2', 'B3'],
+                        'C': ['C0', 'C1', 'C2', 'C3'],
+                        'D': ['D0', 'D1', 'D2', 'D3']},
+                        index=[0, 1, 2, 3])
+
+df2 = pd.DataFrame({'A': ['A4', 'A5', 'A6', 'A7'],
+                        'B': ['B4', 'B5', 'B6', 'B7'],
+                        'C': ['C4', 'C5', 'C6', 'C7'],
+                        'D': ['D4', 'D5', 'D6', 'D7']},
+                         index=[4, 5, 6, 7]) 
+
+df3 = pd.DataFrame({'A': ['A8', 'A9', 'A10', 'A11'],
+                        'B': ['B8', 'B9', 'B10', 'B11'],
+                        'C': ['C8', 'C9', 'C10', 'C11'],
+                        'D': ['D8', 'D9', 'D10', 'D11']},
+                        index=[8, 9, 10, 11])
+
+print(pd.concat([df1, df2, df3]))
+ #     A    B    C    D
+#0    A0   B0   C0   D0
+#1    A1   B1   C1   D1
+#2    A2   B2   C2   D2
+#3    A3   B3   C3   D3
+#4    A4   B4   C4   D4
+#5    A5   B5   C5   D5
+#6    A6   B6   C6   D6
+#7    A7   B7   C7   D7
+#8    A8   B8   C8   D8
+#9    A9   B9   C9   D9
+#10  A10  B10  C10  D10
+#11  A11  B11  C11  D11
+
+esquerda = pd.DataFrame({'key': ['K0', 'K1', 'K2', 'K3'],
+                     'A': ['A0', 'A1', 'A2', 'A3'],
+                     'B': ['B0', 'B1', 'B2', 'B3']})
+   
+direita = pd.DataFrame({'key': ['K0', 'K1', 'K2', 'K3'],
+                          'C': ['C0', 'C1', 'C2', 'C3'],
+                          'D': ['D0', 'D1', 'D2', 'D3']})
+
+print(pd.merge(esquerda, direita))
+#  key   A   B   C   D
+#0  K0  A0  B0  C0  D0
+#1  K1  A1  B1  C1  D1
+#2  K2  A2  B2  C2  D2
+#3  K3  A3  B3  C3  D3
+
+esquerda = pd.DataFrame({'key1': ['K0', 'K0', 'K1', 'K2'],
+                     'key2': ['K0', 'K1', 'K0', 'K1'],
+                        'A': ['A0', 'A1', 'A2', 'A3'],
+                        'B': ['B0', 'B1', 'B2', 'B3']})
+    
+direita = pd.DataFrame({'key1': ['K0', 'K1', 'K1', 'K2'],
+                               'key2': ['K0', 'K0', 'K0', 'K0'],
+                                  'C': ['C0', 'C1', 'C2', 'C3'],
+                                  'D': ['D0', 'D1', 'D2', 'D3']})
+
+print(pd.merge(esquerda, direita, on=['key1', 'key2']))#Vai ser impresso:
+#  key1 key2   A   B   C   D
+#0   K0   K0  A0  B0  C0  D0
+#1   K1   K0  A2  B2  C1  D1
+#2   K1   K0  A2  B2  C2  D2
+
+print(pd.merge(esquerda, direita, how='outer', on=['key1', 'key2']))#Vai ser impresso:
+#  key1 key2    A    B    C    D
+#0   K0   K0   A0   B0   C0   D0
+#1   K0   K1   A1   B1  NaN  NaN
+#2   K1   K0   A2   B2   C1   D1
+#3   K1   K0   A2   B2   C2   D2
+#4   K2   K0  NaN  NaN   C3   D3
+#5   K2   K1   A3   B3  NaN  NaN
+
+print(pd.merge(esquerda, direita, how='left', on=['key1', 'key2']))#Vai ser impresso:
+#  key1 key2   A   B    C    D
+#0   K0   K0  A0  B0   C0   D0
+#1   K0   K1  A1  B1  NaN  NaN
+#2   K1   K0  A2  B2   C1   D1
+#3   K1   K0  A2  B2   C2   D2
+#4   K2   K1  A3  B3  NaN  NaN
+
+print(pd.merge(esquerda, direita, how='right', on=['key1', 'key2']))#Vai ser impresso:
+#  key1 key2    A    B   C   D
+#0   K0   K0   A0   B0  C0  D0
+#1   K1   K0   A2   B2  C1  D1
+#2   K1   K0   A2   B2  C2  D2
+#3   K2   K0  NaN  NaN  C3  D3
+
+esquerda = pd.DataFrame({'A': ['A0', 'A1', 'A2'],
+                     'B': ['B0', 'B1', 'B2']},
+                      index=['K0', 'K1', 'K2']) 
+
+direita = pd.DataFrame({'C': ['C0', 'C2', 'C3'],
+                    'D': ['D0', 'D2', 'D3']},
+                      index=['K0', 'K2', 'K3'])
+
+print(esquerda.join(direita))#Vai ser impresso:
+#     A   B    C    D
+#K0  A0  B0   C0   D0
+#K1  A1  B1  NaN  NaN
+#K2  A2  B2   C2   D2
